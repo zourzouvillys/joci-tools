6e1dcd20c2e0cee4edcf366ac932c7f69f5282de
Theo Zourzouvillys <theo@zrz.io>
2018-06-17 17:38:49 -0700

    additional upload logic

--- client/src/main/java/io/zrz/joci/client/ImageUpload.java
+++ client/src/main/java/io/zrz/joci/client/ImageUpload.java
@@ -27,47 +27,17 @@ public class ImageUpload {
     this.client = new OkHttpClient();
   }
 
-  public Upload upload(Path src) {
-    return upload(MoreFiles.asByteSource(src));
+  public Upload createContext(Path src) {
+    return createContext(MoreFiles.asByteSource(src));
   }
 
-  public Upload upload(ByteSource src) {
+  public Upload createContext(ByteSource src) {
     try {
-
-      Request request = new Request.Builder()
-          .url(registry + "/jars/blobs/uploads/")
-          .post(RequestBody.create(JSON, ""))
-          .build();
-
-      Response response = client.newCall(request).execute();
-
-      if (response.code() / 100 != 2) {
-        throw new IllegalArgumentException("invalid server response");
-      }
-
-      if (!"registry/2.0".equals(response.header("Docker-Distribution-Api-Version", null))) {
-        throw new IllegalArgumentException("invalid server API version");
-      }
-
-      String uploadPath = response.header("location");
-
-      if (uploadPath == null) {
-        throw new IllegalArgumentException("invalid upload path");
-      }
-
-      String uploadId = response.header("docker-upload-uuid", null);
-
-      if (uploadId == null) {
-        throw new IllegalArgumentException("upload ID missing");
-      }
-
-      return new Upload(uploadPath, uploadId, src);
-
+      return new Upload(src);
     }
-    catch (IOException ex) {
-      throw new RuntimeException(ex);
+    catch (IOException e) {
+      throw new RuntimeException(e);
     }
-
   }
 
   public boolean exists(Digest digest) {
@@ -108,16 +78,76 @@ public class ImageUpload {
     private String uploadId;
     private ByteSource src;
     private Digest digest;
+    private Boolean exists = null;
 
-    public Upload(String uploadUrl, String uploadId, ByteSource src) throws IOException {
-      this.uploadUrl = uploadUrl;
-      this.uploadId = uploadId;
+    public Upload(ByteSource src) throws IOException {
       this.src = src;
       this.digest = new Digest(src);
     }
 
-    public void send() {
+    private Upload startUpload() {
+
       try {
+
+        Request request = new Request.Builder()
+            .url(registry + "/jars/blobs/uploads/")
+            .post(RequestBody.create(JSON, ""))
+            .build();
+
+        Response response = client.newCall(request).execute();
+
+        if (response.code() / 100 != 2) {
+          throw new IllegalArgumentException("invalid server response");
+        }
+
+        if (!"registry/2.0".equals(response.header("Docker-Distribution-Api-Version", null))) {
+          throw new IllegalArgumentException("invalid server API version");
+        }
+
+        String uploadPath = response.header("location");
+
+        if (uploadPath == null) {
+          throw new IllegalArgumentException("invalid upload path");
+        }
+
+        String uploadId = response.header("docker-upload-uuid", null);
+
+        if (uploadId == null) {
+          throw new IllegalArgumentException("upload ID missing");
+        }
+
+        this.uploadId = uploadId;
+        this.uploadUrl = uploadPath;
+
+        return this;
+
+      }
+      catch (IOException ex) {
+        throw new RuntimeException(ex);
+      }
+
+    }
+
+    /**
+     * returns false if it was not needed to be uploaded, else true if it was uploaded.
+     * 
+     * checks to ensure it doesn't already exist, if not already checked.
+     * 
+     * @return
+     */
+
+    public boolean send() {
+
+      if (this.exists()) {
+        return false;
+      }
+
+      if (this.uploadId == null) {
+        this.startUpload();
+      }
+
+      try {
+
         HttpUrl url = HttpUrl.parse(uploadUrl).newBuilder().addQueryParameter("digest", digest.toString()).build();
 
         System.err.println("uploading " + url + " " + src.size() + " ---> " + digest.toString());
@@ -137,10 +167,15 @@ public class ImageUpload {
         throw new RuntimeException(ex);
       }
 
+      return true;
+
     }
 
     public boolean exists() {
-      return ImageUpload.this.exists(this.digest);
+      if (this.exists == null) {
+        this.exists = ImageUpload.this.exists(this.digest);
+      }
+      return this.exists;
     }
 
   }
@@ -148,8 +183,7 @@ public class ImageUpload {
   public void uploadFiles(Stream<Path> paths) {
     paths
         .map(MoreFiles::asByteSource)
-        .map(src -> upload(src))
-        .filter(ctx -> !ctx.exists())
+        .map(src -> createContext(src))
         .forEach(ctx -> ctx.send());
   }
 
